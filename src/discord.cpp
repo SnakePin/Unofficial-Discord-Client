b0d80c93814ce89159d33ca21032b92418eaa9ee
Julian Cahill <cahill.julian@gmail.com>
2019-06-19 15:12:58 +1200

    parse websocket HELLO packets

--- src/discord.cpp
+++ src/discord.cpp
@@ -2,6 +2,8 @@
 
 #include "sws/client_wss.hpp"
 
+#include <rapidjson/document.h>
+
 #include <memory>
 
 using namespace Discord;
@@ -15,10 +17,27 @@ void Client::run() {
 	using namespace std;
 	
 	websocket.on_message = [](shared_ptr<WssClient::Connection> connection, shared_ptr<WssClient::InMessage> in_message) {
-		cout << "Client: Message received: \"" << in_message->string() << "\"" << endl;
-
-		// cout << "Client: Sending close connection" << endl;
-		// connection->send_close(1000);
+		std::string response = in_message->string(); // string() can only be called once! see sws/client_ws.hpp for why
+		cout << "Client: Message received: \"" << response << "\"" << endl;
+		
+	
+		rapidjson::Document document;
+		document.Parse(response.c_str());
+		int opcode = document["op"].GetInt();
+		rapidjson::Value eventData = document["d"].GetObject();
+		
+		
+		if(opcode == 10) { // HELLO
+			int heartbeat_interval = eventData["heartbeat_interval"].GetInt();
+			std::cout << "Received HELLO (opcode 10) packet. Heartbeat interval: " << heartbeat_interval << std::endl;
+			
+			// TODO generate and send an IDENTIFY response
+			// TODO respond to heartbeats (opcode 11)
+			// TODO send heartbeat every heartbeat_interval milliseconds
+		}
+		
+		cout << "Client: Sending close connection" << endl;
+		connection->send_close(1000);
 	};
 
 	websocket.on_open = [](shared_ptr<WssClient::Connection> connection) {
