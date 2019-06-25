7d467b210093f25d00951f0c165cc8072da551e5
Julian Cahill <cahill.julian@gmail.com>
2019-06-25 23:08:50 +1200

    snowflake.hpp: removed storing string, added operator overloading.

--- include/discord/snowflake.hpp
+++ include/discord/snowflake.hpp
@@ -7,27 +7,32 @@
 namespace Discord {
 
 	struct Snowflake {
-		std::string original;
 		uint64_t value;
 
-		Snowflake() {
-
-		}
+		Snowflake() : value(0) {}
 
 		void operator=(const Snowflake &other) {
-			original = other.original;
 			value = other.value;
 		}
 
-		Snowflake(std::string s) : original(s), value(0) {
-			std::istringstream iss(original);
+		Snowflake(std::string s) : value(0) {
+			std::istringstream iss(s);
 			iss >> value;
 		}
 
-		Snowflake(uint64_t v) : original(std::to_string(v)), value(v) {}
+		Snowflake(uint64_t v) : value(v) {}
 
+		// Unix epoch in milliseconds
 		inline uint64_t UnixEpoch() {
 			return (value >> 22) + 1420070400000;
+		}
+
+		inline bool operator==(const Snowflake &other) {
+			return other.value == value;
+		}
+
+		inline bool operator!=(const Snowflake &other) {
+			return other.value != value;
 		}
 	};
 
