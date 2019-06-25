f64cc32c055f2ecfd5417ebfe28c0e9dafe8e98b
Julian Cahill <cahill.julian@gmail.com>
2019-06-25 22:28:44 +1200

    use asio::thread_pool to call HTTP requests asynchronously.

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,18 @@
 #include <discord/discord.hpp>
 #include <discord/guild.hpp>
 
-
+#include <asio.hpp>
+#include <asio/thread_pool.hpp>
 
 class MyClient : public Discord::Client {
 public:
 
+	std::shared_ptr<asio::thread_pool> pool; // user created threadpool for making HTTP requests
 	std::vector<Discord::Guild> guilds;
 
-	MyClient(std::string token, Discord::AuthTokenType tokenType) : Client(token, tokenType) {
+	MyClient(std::string token, Discord::AuthTokenType tokenType)
+	    : Client(token, tokenType),
+		pool(std::make_shared<asio::thread_pool>(4)) /* Thread pool contains 4 threads. */ {
 
 	}
 
@@ -20,7 +24,9 @@ public:
 		for(Discord::Channel &chan : g.channels)
 			if(chan.type == 0){
 				Discord::MessagePacket messageToSend{ .content = std::string("Hello to channel #" + g.name), .tts = false };
-				httpAPI.SendMessage(std::to_string(chan.id.value), messageToSend);
+				asio::post(*pool,
+					[=] {httpAPI.SendMessage(std::to_string(chan.id.value), messageToSend);}
+				);
 			}
 	}
 };
@@ -43,16 +49,28 @@ public:
 			client->websocket.stop();
 			std::cout << "Websocket stopped.\n";
 			running = false;
-			return;
 		}
 		else if(command.rfind("sendmsg", 0) == 0) {
 			Discord::MessagePacket messageToSend{ .content = command.substr(command.find(' ')), .tts = false };
 
-			// messageToSend.content = command.substr(command.find(' '));
-
 			//This channel ID is channel ID of test discord guild's general channel's ID
-			client->httpAPI.SendMessage("590695217028661250", messageToSend);
-			return;
+			asio::post(*client->pool, 
+				[=] {
+					client->httpAPI.SendMessage("590695217028661250", messageToSend);
+				}
+			);
+		}
+		else if(command.rfind("delaymsg", 0) == 0) {
+			// Sends a 'typing' signal, then the message after a 5 second delay.
+			Discord::MessagePacket messageToSend{ .content = command.substr(command.find(' ')), .tts = false };
+
+			asio::post(*client->pool,
+				[=] {
+					client->httpAPI.StartTyping("590695217028661250");
+					std::this_thread::sleep_for(std::chrono::seconds(5));
+					client->httpAPI.SendMessage("590695217028661250", messageToSend);
+				}
+			);
 		}
 		else if(command == "guilds") {
 			// Loop through all the guilds and print their names
@@ -63,10 +81,10 @@ public:
 			}
 			std::cout << "\n";
 			
-			return;
 		}
-
-		std::cout << "Unknown command: " << command << "\n";
+		else{
+			std::cout << "Unknown command: " << command << "\n";
+		}
 	}
 
 	void run() {
